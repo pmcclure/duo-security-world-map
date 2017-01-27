460b83478c056a731624b8db603fde26427d4f85
Paul McClure <paulage@gmail.com>
2017-01-26 16:40:04 -0800

    clean up redundant code in app.js

--- app.js
+++ app.js
@@ -10,51 +10,46 @@ var port = process.env.PORT || 3000;
 
 var arr = [];
 
-//Duo variables
+//Duo API variables
 var integrationKey = process.env.DUO_IKEY;
 var secretKey = process.env.DUO_SKEY;
 var host = process.env.DUO_HOST;
 
-var date = new Date().toUTCString();
-//172800 is two days in seconds. i.e get the authentications from the last 2 days. 
-var twoDaysInSeconds = 172800;
-var mintime = Math.floor((new Date().getTime() / 1000) - twoDaysInSeconds).toString();
+const TWO_DAYS_IN_SECONDS = 172800;
+var mintime = Math.floor((new Date().getTime() / 1000) - TWO_DAYS_IN_SECONDS).toString();
 var params = { 'mintime': mintime };
 var client = new duo_api.Client(integrationKey, secretKey, host);
 var duoData = [];
 
-
 function getDuoData() {
-    client.jsonApiCall('GET', '/admin/v1/logs/authentication', params, function (res) {
-    if (res.stat !== 'OK') {
-        console.error('API call returned error: '
-            + res.message);
-        process.exit(1);
-    }
+	client.jsonApiCall('GET', '/admin/v1/logs/authentication', params, function (res) {
+		if (res.stat !== 'OK') {
+			console.error('API call returned error: ' + res.message);
+			process.exit(1);
+		}
 
-    jsonResult = res.response;
-    
-    for (var i = 0; i < jsonResult.length; i++) {
-        jsonResult[i].timestamp = new Date(jsonResult[i].timestamp * 1000).toString();
-    }
+		jsonResult = res.response;
 
-    duoData = jsonResult;    
-})
-}
+		for (var i = 0; i < jsonResult.length; i++) {
+			jsonResult[i].timestamp = new Date(jsonResult[i].timestamp * 1000).toString();
+		}
+
+		duoData = jsonResult;
+	})
+};
 
 getDuoData();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    getDuoData();
-    var displayData = duoData;
+	getDuoData();
 
-    res.render('index', {
-        displayData: duoData
-    })
+	res.render('index', {
+		displayData: duoData
+	})
 });
 
 app.listen(port, () => {
-    console.log(`running on port ${port}`)
+	console.log(`running on port ${port}`)
 });
